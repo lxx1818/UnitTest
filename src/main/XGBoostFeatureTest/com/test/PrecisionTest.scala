713403f21eb14ed2873fe7cd1ba035b4f04a8a01
lxx <1342867021@qq.com>
2019-01-08 19:24:07 +0800

    添加工具类

--- src/main/XGBoostFeatureTest/com/test/PrecisionTest.scala
+++ src/main/XGBoostFeatureTest/com/test/PrecisionTest.scala
@@ -1,8 +1,10 @@
 package com.test
 
+import com.common.OftenUseDataSet.featureColName
 import com.common.{OftenUseDataSet, Tools}
 import org.apache.spark.ml.classification.LogisticRegression
-import org.apache.spark.sql.SparkSession
+import org.apache.spark.ml.feature.{VectorAssembler, XGBoostFeatures}
+import org.apache.spark.sql.{DataFrame, Dataset, SparkSession}
 
 object PrecisionTest {
 
@@ -22,13 +24,75 @@ object PrecisionTest {
   }
 
   def testIris(): Unit ={
+
     val spark = Tools.getSparkSession()
 
     val irisDF = OftenUseDataSet.getGerman(spark)
+    val x = xgAndLR(irisDF)
+    val y = lr(irisDF)
+    val t = 0
+
+  }
+
+  def xgAndLR(dataset: Dataset[_]): Double = {
+
+    val numClass = Tools.getNumClasses(dataset)
+    val objective = if (numClass > 2) "multi:softprob" else "binary:logistic"
+
+    val xgDF = new XGBoostFeatures()
+      .setObjective(objective)
+      .setNumClasses(numClass)
+      .setBoosterType("gbtree")
+      .setEta(0.4)
+      .setGamma(0.1)
+      .setMaxDepth(7)
+      .setMinChildWeight(0.0)
+      .setMaxDeltaStep(0.1)
+      .setSubSample(0.9)
+      .setColSampleByTree(0.9)
+      .setColSampleByLevel(0.9)
+      .setLambda(0.9)
+      .setAlpha(0.1)
+      .setTreeMethod("exact")
+      .setSketchEps(0.04)
+      .setScalePosWeight(0.9)
+      .setSampleType("weighted")
+      .setNormalizeType("forest")
+      .setRateDrop(0.1)
+      .setSkipDrop(0.1)
+      .setRound(100)
+      .setOutPutCol("outputVe")
+      .setFeaturesCol(OftenUseDataSet.featureColName)
+      .setLabelCol(OftenUseDataSet.labelColName)
+      .fit(dataset)
+      .transform(dataset)
+
+    val resultDF = new VectorAssembler()
+      .setInputCols(Array(OftenUseDataSet.featureColName, "outputVe"))
+      .setOutputCol("xgfeature")
+      .transform(xgDF)
 
     val result = new LogisticRegression()
       .setThreshold(0.5)
-      .setMaxIter(30)
+      .setMaxIter(100)
+      .setTol(1.0e-6)
+      .setRegParam(0.0)
+      .setElasticNetParam(0.0)
+      .setFitIntercept(true)
+      .setStandardization(true)
+      .setAggregationDepth(2)
+      .setFeaturesCol("xgfeature")
+      .setLabelCol(OftenUseDataSet.labelColName)
+      .fit(resultDF).transform(resultDF)
+
+    Tools.genAccuracyMC(result)
+  }
+
+  def lr(dataset: Dataset[_]): Double = {
+
+    val result = new LogisticRegression()
+      .setThreshold(0.5)
+      .setMaxIter(100)
       .setTol(1.0e-6)
       .setRegParam(0.0)
       .setElasticNetParam(0.0)
@@ -37,9 +101,9 @@ object PrecisionTest {
       .setAggregationDepth(2)
       .setFeaturesCol(OftenUseDataSet.featureColName)
       .setLabelCol(OftenUseDataSet.labelColName)
-      .fit(irisDF).transform(irisDF)
-    val t = 0
+      .fit(dataset).transform(dataset)
 
+    Tools.genAccuracyMC(result)
   }
 
 }
--- src/main/common/com/common/Tools.scala
+++ src/main/common/com/common/Tools.scala
@@ -1,7 +1,10 @@
 package com.common
 
-import org.apache.spark.ml.feature.VectorAssembler
-import org.apache.spark.sql.{DataFrame, SparkSession}
+import org.apache.spark.ml.attribute._
+import org.apache.spark.ml.evaluation.MulticlassClassificationEvaluator
+import org.apache.spark.ml.feature.{StringIndexer, VectorAssembler}
+import org.apache.spark.sql.types.StructField
+import org.apache.spark.sql.{DataFrame, Dataset, SparkSession}
 
 object Tools {
 
@@ -24,6 +27,40 @@ object Tools {
       .getOrCreate()
   }
 
+  /**
+    * 多分类情况下，预测精度
+    * @param dataset
+    * @return
+    */
+  def genAccuracyMC(dataset: Dataset[_]): Double = {
+    new MulticlassClassificationEvaluator()
+      .setLabelCol(OftenUseDataSet.labelColName)
+      .setPredictionCol("prediction")
+      .setMetricName("accuracy")
+      .evaluate(dataset)
+  }
+
+  def getNumClasses(dataset: Dataset[_]): Int = {
+    GetNumClasses.getNumClasses(dataset.schema(OftenUseDataSet.labelColName)) match {
+      case Some(n: Int) => n
+      case None => dataset.select(OftenUseDataSet.labelColName).distinct().count().toInt
+    }
+  }
+
+}
+
+object GetNumClasses {
+  /**
+    * Examine a schema to identify the number of classes in a label column.
+    * Returns None if the number of labels is not specified, or if the label column is continuous.
+    */
+  def getNumClasses(labelSchema: StructField): Option[Int] = {
+    Attribute.fromStructField(labelSchema) match {
+      case binAttr: BinaryAttribute => Some(2)
+      case nomAttr: NominalAttribute => nomAttr.getNumValues
+      case _: NumericAttribute | UnresolvedAttribute => None
+    }
+  }
 }
 
 object OftenUseDataSet {
@@ -68,8 +105,10 @@ object OftenUseDataSet {
       .setOutputCol(featureColName)
       .transform(dataset)
 
-    resultDF.withColumn(labelColName, resultDF.col(label))
-      .drop(colName: _*)
+    val indexDF = new StringIndexer()
+      .setInputCol(label).setOutputCol(labelColName)
+      .fit(resultDF).transform(resultDF)
+    indexDF.drop(colName: _*)
 
   }
 
