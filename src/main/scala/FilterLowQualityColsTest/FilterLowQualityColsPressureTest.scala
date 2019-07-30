3814f7281018026bc4147c4e37c3b8a69eada39a
lxx <1342867021@qq.com>
2019-07-30 10:19:41 +0800

    测试空值

--- src/main/scala/FilterLowQualityColsTest/FilterLowQualityColsPressureTest.scala
+++ src/main/scala/FilterLowQualityColsTest/FilterLowQualityColsPressureTest.scala
@@ -12,7 +12,14 @@ import scala.io.Source
 object FilterLowQualityColsPressureTest {
 
   def main(args: Array[String]): Unit = {
-    generateCols()
+//    if (args(0).equals("col")) {
+//      testCols()
+//    } else if (args(0).equals("row")){
+//      testRows()
+//    }
+//    generateRows()
+//    testCores(args(0))
+    testNullRatio()
   }
 
   def generateRows(): Unit = {
@@ -20,12 +27,15 @@ object FilterLowQualityColsPressureTest {
 //    val spark = Tools.getSparkSession()
     val colsNum = 10
     val k = 1000
-    val rowsNumArr = Array(1, 10, 100, 1000, 2000, 4000, 6000, 10000)
+    val rowsNumArr = Array(2, 4, 8, 10, 20)
     val fileNames = Array.fill(rowsNumArr.length)("")
     rowsNumArr.indices.foreach { indice =>
-      fileNames(indice) = Tools.generateDataSet(rowsNumArr(indice) * k, colsNum, 100, spark, path = "./rows/")
+      fileNames(indice) = Tools.generateDataSet(rowsNumArr(indice) * k *10000, colsNum, 100, spark, path = "./rows/")
     }
-    Tools.write("./rowsFileName", fileNames)
+    val out = new PrintWriter(new BufferedWriter(new OutputStreamWriter(new FileOutputStream("./rowsFileName", true))), false)
+    out.print(fileNames.mkString("\n"))
+    out.close()
+//    Tools.write("./rowsFileName", fileNames)
   }
 
   def generateCols(): Unit = {
@@ -42,14 +52,27 @@ object FilterLowQualityColsPressureTest {
   }
 
 
+  def generateNullRatio(): Unit = {
+    val spark = SparkSession.builder().appName("gen null data").getOrCreate()
+    //    val spark = Tools.getSparkSession()
+    val nullRatio = Array(0.0, 0.1, 0.2, 0.4, 0.6, 0.8)
+    val k = 1000
+    val rowsNum = 10000 * k
+    val fileNames = Array.fill(nullRatio.length)("")
+    nullRatio.indices.foreach { indice =>
+      fileNames(indice) = Tools.generateDataSet(rowsNum, 100, 100, spark, path = "./nulls/", nullRatio = nullRatio(indice))
+    }
+    Tools.write("./nullsFileName", fileNames)
+  }
+
+
   def testRows(): Unit = {
     val spark = SparkSession.builder().appName("test rows").getOrCreate()
 
-    val source = Source.fromFile("../DataSet/rowsFileName", "UTF-8")
+    val source = Source.fromFile("/home/lxx/DataSet/rowResult", "UTF-8")
     val lineIterator =source.getLines
-    var flag = true
-    for(file <- lineIterator if flag) {
-      val path = "../DataSet/rows/" + file
+    for(file <- lineIterator) {
+      val path = "/home/lxx/DataSet/rows/" + file
       val out = new PrintWriter(new BufferedWriter(new OutputStreamWriter(new FileOutputStream("./rowResult", true))), false)
       out.print(file)
       val df = Tools.readDataFrameASInteger(path, spark).repartition(100)
@@ -61,19 +84,35 @@ object FilterLowQualityColsPressureTest {
       }
       out.println(",rows:" + df.count() + ",cols:" + df.first().length)
       df.unpersist()
-//      flag = false
       out.close
     }
+  }
+
+  def testCores(core: String): Unit = {
+    val out = new PrintWriter(new BufferedWriter(new OutputStreamWriter(new FileOutputStream("./coreResult2", true))), false)
+    val spark = SparkSession.builder().appName("test core").getOrCreate()
+    val path = "/home/lxx/DataSet/rows/rowNum10000000_featureNum10_labelfalse_nullRatio0.0"
+    val df = Tools.readDataFrameASInteger(path, spark).repartition(100)
+    out.print(core)
+    df.cache()
+    df.first()
+    for (i <- 0 to 4) {
+      val time = run(df)
+      out.print("," + time)
+    }
+    out.println(",rows:" + df.count() + ",cols:" + df.first().length)
+    df.unpersist()
+    spark.close()
+    out.close()
   }
 
   def testCols(): Unit = {
     val spark = SparkSession.builder().appName("test cols").getOrCreate()
 
-    val source = Source.fromFile("../DataSet/colsFileName", "UTF-8")
+    val source = Source.fromFile("/home/lxx/DataSet/colsFileName", "UTF-8")
     val lineIterator =source.getLines
-    var flag = true
-    for(file <- lineIterator if flag) {
-      val path = "../DataSet/cols/" + file
+    for(file <- lineIterator) {
+      val path = "/home/lxx/DataSet/cols/" + file
       val out = new PrintWriter(new BufferedWriter(new OutputStreamWriter(new FileOutputStream("./colResult", true))), false)
       out.print(file)
       val df = Tools.readDataFrameASInteger(path, spark).repartition(100)
@@ -85,7 +124,28 @@ object FilterLowQualityColsPressureTest {
       }
       out.println(",rows:" + df.count() + ",cols:" + df.first().length)
       df.unpersist()
-      //      flag = false
+      out.close
+    }
+  }
+
+  def testNullRatio(): Unit = {
+    val spark = SparkSession.builder().appName("test null ratio").getOrCreate()
+
+    val source = Source.fromFile("/home/lxx/DataSet/nullsFileName", "UTF-8")
+    val lineIterator =source.getLines
+    for(file <- lineIterator) {
+      val path = "/home/lxx/DataSet/nulls/" + file
+      val out = new PrintWriter(new BufferedWriter(new OutputStreamWriter(new FileOutputStream("./nullResult", true))), false)
+      out.print(file)
+      val df = Tools.readDataFrameASInteger(path, spark).repartition(100)
+      df.cache()
+      df.first()
+      for (i <- 0 to 4) {
+        val time = run(df)
+        out.print("," + time)
+      }
+      out.println(",rows:" + df.count() + ",cols:" + df.first().length)
+      df.unpersist()
       out.close
     }
   }
